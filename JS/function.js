3ee02120da16c4cb75876bfacb25384760d1c78b
codyswick <47154623+codyswick@users.noreply.github.com>
2019-02-19 21:50:23 -0600

    about section
    added scroll and button effects and about and services section

--- JS/function.js
+++ JS/function.js
@@ -21,3 +21,46 @@ function display(){
   display(x);
 
 }
+
+
+
+/*about toggle*/
+function toggleabout() {
+  let x = document.getElementById("about-us");
+  let y = document.getElementById("services");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = 'none'
+    document.getElementById('about-btn').innerHTML = 'Read Less &raquo';
+  } else {
+    x.style.display = "none";
+    document.getElementById('about-btn').innerHTML = 'Read More &raquo';
+  }
+}
+
+function toggleservices() {
+  let y = document.getElementById("about-us");
+  let x = document.getElementById("services");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "none";
+    document.getElementById('service-btn').innerHTML = 'View Less &raquo';
+  } else {
+    x.style.display = "none";
+    document.getElementById('service-btn').innerHTML = 'View Services &raquo';
+  }
+}
+
+
+
+
+
+/*scroll*/
+
+var controller = new ScrollMagic.Controller();
+
+var scene = new ScrollMagic.Scene({
+  triggerElement: '.row'
+})
+.setClassToggle('.row','show')
+.addTo(controller);
