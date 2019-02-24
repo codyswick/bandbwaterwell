92ec8a3df803f83f8f66758aee5b9844638ac21c
codyswick <47154623+codyswick@users.noreply.github.com>
2019-02-24 14:03:09 -0600

    test

--- contactform.php
+++ contactform.php
@@ -4,13 +4,14 @@ if(isset($_post['submit'])){
   $name = $_post['name'];
   $subject = $_post['subject'];
   $mailFrom = $_post['mail'];
+  $phone = $_post['phone'];
   $message = $_post['message'];
 
   $mailTo = 'codyswick1988@gmail.com'
   $headers = "From: ".$mailFrom;
   $txt = "You have recived an email from ".$name.".\n\n".$message;
 
-  mail($mailTo, $subject, $txt, $headers);
+  mail($mailTo, $subject, $txt, $headers, $phone);
   header("Location:index.html?mailsend");
 }
 ?>
